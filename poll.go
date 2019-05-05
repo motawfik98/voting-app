ef2ff9a496cd92335775b6f710dd264e31bbe24b
Mohamed Ahmed <motawfik1998@gmail.com>
2019-05-05 17:07:26 +0200

    Used the new packages and added new handlers

--- poll.go
+++ poll.go
@@ -3,6 +3,7 @@ package main
 import (
   "github.com/labstack/echo"
   "github.com/labstack/echo/middleware"
+  "./handlers"
 )
 
 func main() {
@@ -15,9 +16,9 @@ func main() {
   e.Use(middleware.Recover())
 
   // Define HTTP routes
-  e.GET("/polls", showPolls)
-  e.PUT("/polls", addPoll)
-  e.PUT("/polls/:id", updatePoll)
+  e.File("/", "public/index.html")
+  e.GET("/polls", handlers.GetPolls(db))
+  e.PUT("/poll/:index", handlers.UpdatePoll(db))
 
   e.Logger.Fatal(e.Start(":9000"))
 }
